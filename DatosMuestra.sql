eca98d58c5061cb4c1cb8a300f2ab2318aa7fe0c
ixonelaguardia <ixone.laguardia@egibide.org>
2016-11-15 16:55:06 +0100

    datos muestra
    Los eventos no funcionan

---
+++ DatosMuestra.sql
@@ -0,0 +1,17 @@
+INSERT INTO pieza VALUES('P00001','Tuerca',2.5,'Gris');
+INSERT INTO pieza VALUES('P00002','Tornillo',1.25,'Rojo');
+INSERT INTO pieza VALUES('P00003','Arandela',3,'Blanco');
+INSERT INTO pieza VALUES('P00004','Clavo',5.5,'Gris');
+INSERT INTO pieza VALUES('P00005','Alcayata',10,'Blanco');
+
+INSERT INTO proveedor VALUES('S00001','Jose','Fernandez','Calle Madrid');
+INSERT INTO proveedor VALUES('S00002','Manuel','Vidal','Avenida Londres');
+INSERT INTO proveedor VALUES('S00003','Luisa','Gomez','Alameda Lisboa');
+INSERT INTO proveedor VALUES('S00004','Pedro','Sanchez','Pasaje Paris');
+INSERT INTO proveedor VALUES('S00005','Maria','Reyes','Canton Roma');
+
+INSERT INTO proyecto VALUES('J00001','proyecto 1','Londres');
+INSERT INTO proyecto VALUES('J00002','Proyecto 2','Londres');
+INSERT INTO proyecto VALUES('J00003','Proyecto 3','Paris');
+INSERT INTO proyecto VALUES('J00004','Proyecto 4','Roma');
+
--- GestorProyectos/src/gestorproyectos/GestorProyectos.java
+++ GestorProyectos/src/gestorproyectos/GestorProyectos.java
@@ -5,17 +5,54 @@
  */
 package gestorproyectos;
 
+import GUI.Principal;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Ixone
  */
 public class GestorProyectos {
 
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         // TODO code application logic here
+        
+        //Cargar el driver
+	Class.forName("com.mysql.jdbc.Driver");
+        Connection conexion;
+        
+        
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        /*      PRUEBA CONEXION
+       
+      		 //Cargar el driver
+		 Class.forName("com.mysql.jdbc.Driver");
+		 // Establecemos la conexion con la BD
+		 Connection conexion  = (Connection) DriverManager.getConnection("jdbc:mysql://192.168.33.10/scotchbox","root", "root");
+  
+
+		 // Preparamos la consulta
+		 Statement sentencia = (Statement) conexion.createStatement();
+		 ResultSet result = sentencia.executeQuery ("SELECT * FROM pieza");
+                 while(result.next()){
+                    System.out.println(result.getString("codigo")+" " +result.getFloat("precio")+" " +result.getString("descripcion"));
+		 }
+                         
+                 
+                 result.close();
+		 sentencia.close();
+                 conexion.close();
+         */
     }
-    
 }
